ce6418b881433c34f9ffff26979e7288402cefb4
김혜정(molly.ouo) <molly.ouo@kakaocorp.com>
2024-02-02 19:41:33 +0900

    버튼에 margin-top 추가

--- components/WriteDiary.vue
+++ components/WriteDiary.vue
@@ -103,7 +103,7 @@ const showResult = () => {
         </div>
         <nuxt-link to="/result">
           <button
-            class="text-3xl inline-flex items-center justify-center rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground h-10 px-4 py-2 w-full bg-indigo-500 hover:bg-indigo-700"
+            class="mt-5 text-3xl inline-flex items-center justify-center rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground h-10 px-4 py-2 w-full bg-indigo-500 hover:bg-indigo-700"
             type="submit"
             :disabled="!isValid"
             @click="showResult"
