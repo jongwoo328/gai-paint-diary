ee7fdee28c1205663880f0f53eea30e90f87a930
김혜정(molly.ouo) <molly.ouo@kakaocorp.com>
2024-02-02 19:23:33 +0900

    덜 완성된 네비게이션바 추가

---
+++ components/Navigation.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import ListIcon from "~/components/button/ListIcon.vue";
+import HomeIcon from "~/components/button/HomeIcon.vue";
+</script>
+
+<template>
+  <div class="flex flex-row items-center justify-center p-6">
+    <div class="has-tooltip">
+      <span class="tooltip rounded shadow-lg p-2 bg-white -mb-8">메인화면으로 돌아가기</span>
+      <HomeIcon class="w-6 mr-5" />
+    </div>
+    <div class="has-tooltip">
+      <span class="tooltip rounded shadow-lg p-2 bg-white -mb-8">결과 리스트 보기</span>
+      <span><ListIcon class="w-4" /></span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.tooltip {
+  @apply invisible absolute;
+  cursor: default;
+}
+
+.has-tooltip:hover .tooltip {
+  @apply visible z-auto;
+  cursor: pointer;
+}
+</style>
