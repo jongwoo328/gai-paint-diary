934e67269f5cd2ea54b874aefe425a8a934c2cc9
justin.dev <justin.dev@kakaocorp.com>
2024-02-06 10:48:23 +0900

    테스트용 데이터 삭제

--- pages/list.vue
+++ pages/list.vue
@@ -1,37 +1,10 @@
 <script setup lang="ts">
 import type { DiaryListItem } from "~/types";
-import { Mood } from "~/shared/types/request";
 import { useDiaryStore } from "~/stores/DiaryStore";
 
 const { setSelectedDiary } = useDiaryStore();
 
-const diaries = useLocalStorage<DiaryListItem[]>("diaryList", [
-  // 테스트를 위한 정보, 실제로는 [] 로 사용
-  {
-    date: "2024-02-02",
-    content:
-      "오늘은 금요일에 쉬는 날이다. 혼자 카페에서 노트북을 펴고 프로젝트를 진행하고 있다.\n" +
-      "카페안의 사람들은 평화롭게 앉아있다.",
-    imageUrl: "https://dummyimage.com/1024x1024/fff/000",
-    mood: Mood.Happiness,
-  },
-  {
-    date: "2024-02-02",
-    content:
-      "오늘은 금요일에 쉬는 날이다. 혼자 카페에서 노트북을 펴고 프로젝트를 진행하고 있다.\n" +
-      "카페안의 사람들은 평화롭게 앉아있다.",
-    imageUrl: "https://dummyimage.com/1024x1024/fff/000",
-    mood: Mood.Happiness,
-  },
-  {
-    date: "2024-02-02",
-    content:
-      "오늘은 금요일에 쉬는 날이다. 혼자 카페에서 노트북을 펴고 프로젝트를 진행하고 있다.\n" +
-      "카페안의 사람들은 평화롭게 앉아있다.",
-    imageUrl: "https://dummyimage.com/1024x1024/fff/000",
-    mood: Mood.Happiness,
-  },
-]);
+const diaries = useLocalStorage<DiaryListItem[]>("diaryList", []);
 
 const onClickItem = (diary: DiaryListItem) => {
   setSelectedDiary(diary);
